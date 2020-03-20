5eb6a29479c563e9cd2c36b5d309037d210f453b
luigiberducci <luigi.berd@gmail.com>
2020-03-20 13:27:16 +0100

    update plotevent with plot tracking + scattering according to Edep, instead of simple scattering

--- Development/plot_event.py
+++ Development/plot_event.py
@@ -1,64 +1,87 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.patches as mpatches
 import argparse
-import math
 import os
+import warnings
 
-particles = {'m': 13, 'n':2112, 'g':22}  # dict particle names -> ids
+particles = {'e-': 11, 'e+': -11, 'm': 13, 'n': 2112, 'g': 22}  # dict particle names -> ids
+labels = {'e-': 'electron', 'e+': 'positron', 'm': 'muon', 'n': 'neutron', 'g': 'photon'}  # dict particle id->name
+colors = {'e-': 'r', 'e+': 'b', 'm': 'orange', 'n': 'y', 'g': 'g'}   # dict particle colors
+edep_scale = 0.1    # scaling factor to avoid "huge" markers
 
+default_infile = os.path.join("..", "Data", "output10000.csv")
 parser = argparse.ArgumentParser()
+parser.add_argument("--infile", type=argparse.FileType('r'), default=default_infile, help="Input filepath")
 parser.add_argument("eventnumber", type=int, default=0, nargs='?', help="Event number (id)")
 args = parser.parse_args()
 
-event=args.eventnumber
-plot_interval = 10000
-file_path = os.path.join("..", "Data", "output123456789.csv")
-file_path = os.path.join("..", "Data", "output10000.csv")
-df = pd.read_csv(file_path, index_col=False)
-df = df[df['eventnumber'] == event]
-print("[Info] Event {} loaded. All entries: {}\nPlot only muons, electrons, neutrons, photons".format(event, len(df)))
+file_path = args.infile
+print("[Info]\tLoad file {}".format(file_path.name))
+full_df = pd.read_csv(file_path, index_col=False)
+eventnumbers = full_df.eventnumber.unique()
+print("[Info]\tLoaded. Num events: {}\nAvailable event numbers:\n{}".format(len(eventnumbers), eventnumbers))
 
-threedee = plt.figure().gca(projection='3d')
-threedee.set_xlabel('X')
-threedee.set_ylabel('Y')
-threedee.set_zlabel('Z')
+while True:
+    try:
+        event = int(input("[Input]\tInsert event number or 'q'..."))
+        if event not in eventnumbers:
+            warnings.warn("no event {} in file {}".format(event, file_path.name))
+    except ValueError:
+        break
+    df = full_df[full_df['eventnumber'] == event]
+    available_pid = list(df.PID.unique())
+    selected_pid = sorted(set(particles.values()).intersection(available_pid))
 
-# define sipm topology
-n_sipm = 30     # num sipm for top/bottom
-alphas = np.linspace(0, 2*math.pi, n_sipm)
-radius = 1500
-x = 0 + radius * np.cos(alphas)  # x-coord
-z = 0 + radius * np.sin(alphas)  # z-coord, y-coord is fixed for top and bottom
-y_top = + 1500
-y_bottom = - 1500
-# threedee.scatter(x, y_top, z, c="orange")
-# threedee.scatter(x, y_bottom, z, c="orange")
+    print("[Info]\tEvent {} loaded.\tNum entries: {}".format(event, len(df)))
+    print("[Info]\tFound particles: {}\n\tPlot particles: {}".format(available_pid, selected_pid))
 
-for i in range(len(df)):
-    x = df.iloc[i,:]['x']
-    y = df.iloc[i,:]['y']
-    z = df.iloc[i,:]['z']
-    pid = df.iloc[i,:]['PID']
-   # parent = df.iloc[i,:]['PID_parent']
-    color = "black"    # all particles
-    if pid == 11:
-        color = "red"  # electrons
-    elif pid == 13:
-        color = "blue"  # muon
-    elif pid == 22:
-        color = "green"   # gamma
-    elif pid == 2112:
-        color = "black" # neutron
-    # if pd.isna(parent) and pid!=13: #not muon
-    #     color = "purple"
-    if color=="black":
-        continue
-    threedee.scatter(x, y, z, c=color)
-    if i>0 and i % plot_interval == 0:
-        plt.pause(0.001)
-threedee.set_xlim(-1500,1500)
-threedee.set_ylim(-1500,1500)
-threedee.set_zlim(-1500,1500)
-plt.show()
+    threedee = plt.figure().gca(projection='3d')
+    threedee.set_title('Event {} from {}'.format(event, file_path.name))
+    threedee.set_xlabel('X')
+    threedee.set_ylabel('Y')
+    threedee.set_zlabel('Z')
+    handles = []
+    for id in labels.keys():
+        patch = mpatches.Patch(color=colors[id], label=labels[id])
+        handles.append(patch)
+    threedee.legend(handles=handles)
+
+    df_gb = df.sort_values("time").groupby("PID")
+    muons = df_gb.get_group(particles["m"]) if particles['m'] in selected_pid else pd.DataFrame()
+    posit = df_gb.get_group(particles["e+"]) if particles['e+'] in selected_pid else pd.DataFrame()
+    elect = df_gb.get_group(particles["e-"]) if particles['e-'] in selected_pid else pd.DataFrame()
+    neutr = df_gb.get_group(particles["n"]) if particles['n'] in selected_pid else pd.DataFrame()
+    gamma = df_gb.get_group(particles["g"]) if particles['g'] in selected_pid else pd.DataFrame()
+
+    # plot muon trajectory (main trajectory)
+    if not muons.empty:
+        threedee.plot(muons.x, muons.y, muons.z, c=colors['m'])
+    # the other particles, plot each single track
+    if not elect.empty:
+        elect_gb = elect.groupby("tracknumber")
+        elect_gb.apply(lambda track: threedee.plot(track.x, track.y, track.z, c=colors['e-']))
+        elect_gb.apply(lambda track: threedee.scatter(track.x, track.y, track.z, c=colors['e-'],
+                                                      marker='*', s=edep_scale*track.energydeposition))
+    if not posit.empty:
+        posit_gb = posit.groupby("tracknumber")
+        posit_gb.apply(lambda track: threedee.plot(track.x, track.y, track.z, c=colors['e+']))
+        posit_gb.apply(lambda track: threedee.scatter(track.x, track.y, track.z, c=colors['e+'],
+                                                      marker='*', s=edep_scale*track.energydeposition))
+    if not neutr.empty:
+        neutr_gb = neutr.groupby("tracknumber")
+        neutr_gb.apply(lambda track: threedee.plot(track.x, track.y, track.z, c=colors['n']))
+        neutr_gb.apply(lambda track: threedee.scatter(track.x, track.y, track.z, c=colors['n'],
+                                                      marker='*', s=edep_scale*track.energydeposition))
+    if not gamma.empty:
+        gamma_gb = gamma.groupby("tracknumber")
+        gamma_gb.apply(lambda track: threedee.plot(track.x, track.y, track.z, c=colors['g']))
+        gamma_gb.apply(lambda track: threedee.scatter(track.x, track.y, track.z, c=colors['g'],
+                                                      marker='*', s=edep_scale*track.energydeposition))
+    threedee.set_xlim(-1950, +1950)
+    threedee.set_ylim(-1950, +1950)
+    threedee.set_zlim(-1950, +1950)
+    plt.show()
+
+print("[Info] End.")
