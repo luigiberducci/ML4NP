927f686ac0572981088013e904e6ab42897b0ec4
luigiberducci <luigi.berd@gmail.com>
2020-04-06 20:08:19 +0200

    problem in merge: warning outfile not closed

--- Development/Preprocessing.cpp
+++ Development/Preprocessing.cpp
@@ -8,79 +8,138 @@
 #include <TROOT.h>
 #include <TSystem.h>
 
+#define TmpROIFilePrefix "tmproi"
+#define OutFilePrefix "roi"
+#define CJSimFilePrefix "output"
+
 using namespace std;
 
-void data_cleaning(const char * dirIn, const char * dirOut, const char * mapDir){
-    cout << "[Info] Data cleaning...\n";
-    // IO directories
-    char* fullDirIn = gSystem->ExpandPathName(dirIn);
-    char* fullDirOut = gSystem->ExpandPathName(dirOut);
+TH3D* getOpticalMap(const char * mapDir){
     char* fullMapDir = gSystem->ExpandPathName(mapDir);
-    // Loop files in input dir
-    void* dirp = gSystem->OpenDirectory(fullDirIn);
-    const char* entry;
     // Get Neil's map
     TString mapFileName = "OpticalMapL200XeD.14String.5mm";
     TFile* mapFile = TFile::Open(fullMapDir + mapFileName + TString(".root"));
     TH3D *hMap;
     mapFile->GetObject("ProbMapInterior", hMap);
+    return hMap;
+}
 
-    TTree *mergedTree;
-    TList *listTree = new TList;
-    Long64_t k_entries = 0, entry_x_file = 1000000;
-    Int_t id_part = 0;
-    while((entry = (char*)gSystem->GetDirEntry(dirp))) {
-        // Consider files `outputXXXXXX.root`
-        TString fileName = entry;
-        TString begin = fileName(0, 6);
-        if(!(begin.EqualTo("output") & fileName.EndsWith("root")))
-            continue;
-        std::cout << "\t[Info] Processing " << dirIn + fileName << endl;
-        // Open input file and take original tree
-        TFile* file = TFile::Open(dirIn + fileName);
-        TTree* originalTree = (TTree*)file->Get("fTree");
-        TFile* output = TFile::Open(dirOut + fileName.Copy().Replace(0, 6, "cutROI"), "RECREATE");
-        // cut: ROI & Edep>0
-        // ROI def: x, y in [-500, +500], z in [-1000, +1000]
-        TString cutX("(x >= -500.) & (x <= 500.)");
-        TString cutY("(y >= -500.) & (y <= 500.)");
-        TString cutZ("(z >= -1000) & (z <= 1000)");
-        TString cutE("(energydeposition > 0)");
-        // Copy tree
-        TTree* reducedTree = originalTree->CopyTree(cutX + " & " + cutY + " & " + cutZ);
+bool isSimulationFile(TString fileName){
+    // Consider files `outputXXXXXX.root`
+    TString begin = fileName(0, 6);
+    if(begin.EqualTo(CJSimFilePrefix) & fileName.EndsWith("root"))
+        return true;
+    return false;
+}
 
-        // add branch detection efficiency from Neil's map
-        Double_t x, y, z, detectionefficiency;
-        TBranch *bDEff = reducedTree->Branch("detectionefficiency",
-                                              &detectionefficiency,
-                                              "detectionefficiency/D");
-        reducedTree->SetBranchAddress("x",&x);
-        reducedTree->SetBranchAddress("y",&y);
-        reducedTree->SetBranchAddress("z",&z);
-        for (Long64_t i = 0; i < reducedTree->GetEntries(); i++) {
-            reducedTree->GetEntry(i);
-            Int_t bin = hMap->FindBin(x, y, z);
-            detectionefficiency = hMap->GetBinContent(bin);
-            if(detectionefficiency>1){
-                cout << "i: " << i << ", ";
-                cout << "x: " << x << ", ";
-                cout << "y: " << y << ", ";
-                cout << "z: " << z << ", ";
-                cout << "deff: " << detectionefficiency << endl;
-            }
-            bDEff->Fill();
-        }
-        // save cleaned files
-        cout << "\t[Info] original entries: " << originalTree->GetEntries() << ", ";
-        cout << "ROI entries: " << reducedTree->GetEntries() << ", ";
-        cout << "perc cut: " << (1 - ((double)reducedTree->GetEntries() / originalTree->GetEntries())) << " %\n\n";
+bool isTmpROIFile(TString fileName){
+    // Consider files `tmproiXXXXXX.root`
+    TString begin = fileName(0, 6);
+    if(begin.EqualTo(TmpROIFilePrefix) & fileName.EndsWith("root"))
+        return true;
+    return false;
+}
 
-        // merge files based on number of entries
-        listTree->Add(reducedTree);
+TTree* getReducedTree(const char * dirIn, const char * dirOut, TString fileName){
+    char* fullDirIn = gSystem->ExpandPathName(dirIn);
+    // Open input file and take original tree
+    TFile* file = TFile::Open(dirIn + fileName);
+    TFile* output = TFile::Open(dirOut + fileName.Copy().Replace(0, 6, TmpROIFilePrefix), "RECREATE");
+    TTree* originalTree = (TTree*)file->Get("fTree");
+    // cut: ROI & Edep>0
+    TString cutX("(x >= -500.) & (x <= 500.)");
+    TString cutY("(y >= -500.) & (y <= 500.)");
+    TString cutZ("(z >= -1000) & (z <= 1000)");
+    TString cutE("(energydeposition > 0)");
+    TTree* reducedTree = originalTree->CopyTree(cutX + " & " + cutY + " & " + cutZ);
+    reducedTree->SetDirectory(output);      // otherwise remain related to infile
+    delete originalTree;    // I cannot delete file, otwise seg fault
+    return reducedTree;
+}
 
+void addDetEfficiencyBranch(TTree * reducedTree, TH3D * hMap){
+    Double_t x, y, z, detectionefficiency;
+    TBranch *bDEff = reducedTree->Branch("detectionefficiency",
+                                         &detectionefficiency,
+                                         "detectionefficiency/D");
+    reducedTree->SetBranchAddress("x",&x);
+    reducedTree->SetBranchAddress("y",&y);
+    reducedTree->SetBranchAddress("z",&z);
+    for (Long64_t i = 0; i < reducedTree->GetEntries(); i++) {
+        reducedTree->GetEntry(i);
+        Int_t bin = hMap->FindBin(x, y, z);
+        detectionefficiency = hMap->GetBinContent(bin);
+        bDEff->Fill();
     }
+}
+
+void data_cleaning(const char * dirIn, const char * dirOut, const char * mapDir){
+    cout << "[Info] Data cleaning: Only ROI and Edep>0. Add OP DetEff.\n";
+    // IO directories
+    char* fullDirIn = gSystem->ExpandPathName(dirIn);
+    char* fullDirOut = gSystem->ExpandPathName(dirOut);
+    TH3D* hMap = getOpticalMap(mapDir);
+
+    void* dirp = gSystem->OpenDirectory(fullDirIn);
+    const char* entry;
+    while((entry = (char*)gSystem->GetDirEntry(dirp))) {
+        TString fileName = entry;
+        if(!isSimulationFile(fileName))    continue;
+        cout << "\t" << dirIn + fileName << endl;
+
+        TFile* output = TFile::Open(dirOut + fileName.Copy().Replace(0, 6, TmpROIFilePrefix), "RECREATE");
+        TTree* reducedTree = getReducedTree(fullDirIn, fullDirOut, fileName);
+        addDetEfficiencyBranch(reducedTree, hMap);
+        reducedTree->Write();
+        output->Close();
+        delete reducedTree;
+        delete output;
+    }
+    delete hMap;
     gSystem->FreeDirectory(dirp);   // Free pointer to dir
     cout << "[Info] Data cleaning: completed.\n";
+}
+
+void mergeChain(TChain* ch, const char * dirOut, int id_group){
+    TString fileOut(dirOut);
+    fileOut += OutFilePrefix;
+    fileOut += "_part";
+    fileOut += id_group;
+    fileOut += ".root";
+    ch->Merge(fileOut);
+    // Debug
+    cout << "\tMerged " << ch->GetEntries() << " entries in " << fileOut << endl << endl;
+}
+
+void compact_data(const char * dirIn, const char * dirOut){
+    cout << "[Info] Merge data...\n";
+    char* fullDirIn = gSystem->ExpandPathName(dirIn);
+    char* fullDirOut = gSystem->ExpandPathName(dirOut);
+    void* dirp = gSystem->OpenDirectory(fullDirIn);
+    const char* entry;
+    TChain* ch = new TChain("fTree");
+    Long64_t k_entries = 0, entry_x_file = 2000000;
+    Int_t id_group = 0;
+    while((entry = (char*)gSystem->GetDirEntry(dirp))) {
+        TString fileName = entry;
+        if(!isTmpROIFile(fileName))    continue;
+        cout << "\t" << dirIn + fileName << endl;
+
+        if(k_entries >= entry_x_file){
+            mergeChain(ch, fullDirOut, ++id_group);
+            k_entries = 0;     // reset counter
+            ch->Reset();
+        }
+        ch->Add(dirIn + fileName);
+        k_entries = ch->GetEntries();
+    }
+    if(k_entries > 0){
+        mergeChain(ch, fullDirOut, ++id_group);
+        ch->Reset();
+    }
+    delete ch;
+    gSystem->FreeDirectory(dirp);   // Free pointer to dir
+    cout << "[Info] Merge data: completed.\n";
 }
 
 void data_preparation(){
@@ -96,6 +155,7 @@ int main(){
     const char * dirOut = "/home/luigi/Development/ML4NP/Development/Out/";
     const char * mapDir = "/home/luigi/Development/ML4NP/Data/root/";
     data_cleaning(dirIn, dirOut, mapDir);
+    compact_data(dirOut, dirOut);
     // Data preparation
     data_preparation();
     cout << "[Info] End.\n";
