ce4edc82feeb4be26dc91c142e735a245ef191cc
luigiberducci <luigi.berd@gmail.com>
2020-05-07 09:50:03 +0200

    start working on snapshot dataset creator

--- Development/SnapshotCreator.cpp
+++ Development/SnapshotCreator.cpp
@@ -52,7 +52,7 @@ bool isOutFile(TString fileName){
     return isRootFile(fileName, prefix);
 }
 
-TString createDatasetOutFile(const char * dirOut, TString prefixOut, Double_t nDeltaT, Double_t DeltaTns){
+TString createDatasetFilename(const char * dirOut, TString prefixOut, Double_t nDeltaT, Double_t DeltaTns){
     char* fullDirOut = gSystem->ExpandPathName(dirOut);
     TString outFileName = fullDirOut + prefixOut + "_T";
     outFileName += nDeltaT * DeltaTns;
@@ -94,21 +94,24 @@ TEntryList* getEntryListOfEvent(TTree *fTree, Int_t eventnumber){
 
 void produce_time_dataset(const char * dirIn, const char * dirOut, TString prefixIn, TString prefixOut){
     // Parameters
-    Double_t DeltaT = 2;    // integration time
+    Double_t DeltaT = 2;    // integration time (ns)
     Int_t nDeltaT = 40;     // number of successive integrations
     int num_shiftings = 100;	// Number of instances for each event
     int min_shifting = 0;	// Interval shifting (min)
     int max_shifting = 100;	// Interval shifting (max)
-    // Let's go
-    cout << "[Info] Create dataset wt T=" << nDeltaT * DeltaTns << ", dT=" << DeltaTns << "...\n";
-    char* fullDirIn = gSystem->ExpandPathName(dirIn);
-    // Create output file stream
+    // Debug
+    cout << "[Info] From files " << dirIn << "/" << prefixIn << "*" <<  endl;
+    cout << "[Info] Create snapshot of T=" << nDeltaT * DeltaT << " ns, ";
+    cout << "dT=" << DeltaT << "\n";
+    // IO management
     ofstream outCSV;
-    TString outFile(createDatasetOutFile(dirOut, prefixOut, nDeltaT, DeltaTns));
+    TString outFile(createDatasetFilename(dirOut, prefixOut, nDeltaT, DeltaTns));
     outCSV.open(outFile);
     // Write CSV header: number of Dt, Dt in ns, resulting time T
     outCSV << nDeltaT << "," << DeltaTns << "," << nDeltaT * DeltaTns << "\n";
+
     // Loop files in input directory
+    char* fullDirIn = gSystem->ExpandPathName(dirIn);
     void* dirp = gSystem->OpenDirectory(fullDirIn);
     const char* entry;
     while((entry = (char*)gSystem->GetDirEntry(dirp))) {
@@ -120,8 +123,7 @@ void produce_time_dataset(const char * dirIn, const char * dirOut, TString prefi
         TTree *fTree = (TTree*) f->Get("fTree");
         TParameter<Int_t> *NSiPM = (TParameter<Int_t>*) f->Get("NSiPM");
         const int nSiPM = (const int) NSiPM->GetVal();
-        // Collect distinct event numbers
-        set<Int_t> eventnumbers = getSetOfEventNumbers(fTree);
+
         // Connect branches
         Int_t eventnumber;
         Double_t time;
@@ -134,23 +136,26 @@ void produce_time_dataset(const char * dirIn, const char * dirOut, TString prefi
             fTree->SetBranchAddress(branchName, &SiPM[sipm]);
         }
         // Loop over events
+        set<Int_t> eventnumbers = getSetOfEventNumbers(fTree);
         for(auto event : eventnumbers){
-            // Create dataset instance struct
+	    // TODO introduce shifting here!
+            // Create empty snapshot struct
             vector<vector<Long64_t>> TSiPMEvent = newDatasetEventInstance(nSiPM, nDeltaT);
             // Loop over event entries
             TEntryList *elist = getEntryListOfEvent(fTree, event);
             Long64_t eventEntry;
             while((eventEntry = elist->Next()) >= 0){   // when list ends -1
                 fTree->GetEntry(eventEntry);
-                int idDTSnapshot = floor((time + shift) / DeltaTns);
-                if(idDTSnapshot < 0 || idDTSnapshot >= nDeltaT)
+                int id_time_bin = floor((time + min_shifting) / DeltaT);
+                if(id_time_bin < 0 || id_time_bin >= nDeltaT)
                     continue;   // all the others are bigger than time T (eventually overflow)
-                // Integrate according to Dt
+                // Integrate in the time bin
                 for(int sipm = 0; sipm < nSiPM; sipm++){
                     TSiPMEvent[idDTSnapshot][sipm] += SiPM[sipm];
                 }
             }
             // Write the event to the out csv file
+	    // TODO: add check empty snapshot (all zero non write it)
             for(auto snapshot : TSiPMEvent){
                 for(auto sipm : snapshot)
                     outCSV << sipm << ",";
@@ -169,14 +174,12 @@ void produce_time_dataset(const char * dirIn, const char * dirOut, TString prefi
 
 int main(){
     cout << "[Info] Snapshot Dataset Creator...\n";
-    // Data cleaning
-    const char * dirIn = "/home/data/Ar39/";
-    const char * dirOut = "/home/data/Ar39Preproc/";
+    // LGND Docker filesystem
+    // const char * dirIn = "/home/data/Ar39/";
+    // const char * dirOut = "/home/data/Ar39Preproc/";
     // Local
-    // const char * dirIn = "Data/";
-    // const char * dirOut = "Out/";
-    // Data cleaning
-    Long64_t entry_x_file = 3000000;	//Compact root files to have this number of entries
-    //produce_time_dataset(dirOut, dirOut, TmpSiPMFilePrefix, "dataset");
+    const char * dirIn = "./";
+    const char * dirOut = "Out/";
+    produce_time_dataset(dirIn, dirOut, "SiPMTrace_Ar39_", "Ar39_Snapshots_");
     cout << "[Info] End.\n";
 }
