04341fa605812ae268b7dc3c38c6a1da986f7fc3
Luigi Berducci <luigi.berd@gmail.com>
2020-05-21 10:14:03 +0200

    update preproc

--- Development/Preprocessing.cpp
+++ Development/Preprocessing.cpp
@@ -18,6 +18,7 @@
 
 #define RNDSEED 123456789
 #define PI 3.14159265
+#define EVENTxFILE 20000
 
 #define CJSimFilePrefix "output"
 #define TmpROIFilePrefix "tmproi"
@@ -76,24 +77,30 @@ bool isOutFile(TString fileName){
 
 pair<TFile*, TTree*> getReducedTree(const char * dirIn, const char * dirOut, TString fileName){
     TFile *file, *output;
-    TTree *originalTree, *reducedTree;
+    TTree *originalTree, *reducedTree, *checkTree;
     char* fullDirIn = gSystem->ExpandPathName(dirIn);
     // Open input file and take original tree
     file = TFile::Open(dirIn + fileName);
     output = TFile::Open(dirOut + fileName.Copy().Replace(0, 6, TmpROIFilePrefix), "RECREATE");
     originalTree = (TTree*)file->Get("fTree");
+    // Check Event consistency
+    TString cutEvent("eventnumber > ");  // If this cut has some entries -> BREAK!
+    cutEvent += EVENTxFILE;
+    checkTree = originalTree->CopyTree(cutEvent);
+    assert(checkTree->GetEntries() == 0); // Assert: risk event overlap
     // cut: ROI & Edep>0
     TString cutX("(x >= -700.) & (x <= 700.)");
     TString cutY("(y >= -700.) & (y <= 700.)");
-    TString cutZ("(z >= -845) & (z <= 845)");
-    TString cutE("(energydeposition > 0)");
+    TString cutZ("(z >= -845.) & (z <= 845.)");
+    //TString cutE("(energydeposition > 0)");
     // cut original tree
     reducedTree = originalTree->CopyTree(cutX + " & " + cutY + " & " + cutZ);
     // Debug
-    // cout << "[Cut ROI] File: " << fileName << ", original entries: " << originalTree->GetEntries() << ", reduced entries: " << reducedTree->GetEntries() << endl;
-    originalTree->Delete();    // I cannot delete file, otwise seg fault
+    cout << " | Cut ROI: original entries: " << originalTree->GetEntries() << ", reduced entries: " << reducedTree->GetEntries();
+    originalTree->Delete();    // I cannot delete output file, otwise seg fault
     file->Close();
     file->Delete();
+    checkTree->Delete();
     return make_pair(output, reducedTree);
 }
 
@@ -128,7 +135,7 @@ void addBranches(TTree * reducedTree, TH3D * hMap, TH2D * oMap, Int_t event_x_fi
 }
 
 void data_cleaning(const char * dirIn, const char * dirOut, const char * mapDir){
-    cout << "[Info] Data cleaning: Only ROI and Edep>0. Add OP DetEff.\n";
+    cout << "[Info] Data cleaning: Only ROI. Add OP DetEff.\n";
     // IO directories
     char* fullDirIn = gSystem->ExpandPathName(dirIn);
     char* fullDirOut = gSystem->ExpandPathName(dirOut);
@@ -140,7 +147,7 @@ void data_cleaning(const char * dirIn, const char * dirOut, const char * mapDir)
 
     void* dirp = gSystem->OpenDirectory(fullDirIn);
     const char* entry;
-    Int_t event_x_file = 10000, input_file_counter = 0;	// to solve the repeatition of event number in input files
+    Int_t event_x_file = EVENTxFILE, input_file_counter = 0;	// to solve the repeatition of event number in input files
     while((entry = (char*)gSystem->GetDirEntry(dirp))) {
         TString fileName = entry;
         if(!isSimulationFile(fileName))    continue;
@@ -235,7 +242,6 @@ void convert_to_sliced_detections(const char * dirIn, const char * dirOut){
     const char* entry;
     set<Int_t> original_events;
     set<Int_t> produced_events;
-    set<Int_t> discarded_events;
     Int_t filePartID = 0;
     while((entry = (char*)gSystem->GetDirEntry(dirp))) {
         TString fileName = entry;
@@ -301,8 +307,6 @@ void convert_to_sliced_detections(const char * dirIn, const char * dirOut){
             if(opDetected > 0){
                 SiPMTree.Fill();
                 produced_events.insert(eventnumber);
-            }else{
-                discarded_events.insert(eventnumber);
             }
         }
 	// Debug
@@ -342,8 +346,8 @@ int main(){
     // const char * mapDir = "../Data/OpticalMaps/";
     // Data cleaning
     Long64_t entry_x_file = 20000000;	//Compact root files to have this number of entries
-    data_cleaning(dirIn, dirOut, mapDir);
-    compact_data(dirOut, dirOut, TmpROIFilePrefix, OutROIFilePrefix, entry_x_file);
+    // data_cleaning(dirIn, dirOut, mapDir);
+    // compact_data(dirOut, dirOut, TmpROIFilePrefix, OutROIFilePrefix, entry_x_file);
     // Data preparation
     convert_to_sliced_detections(dirOut, dirOut);
     cout << "[Info] End.\n";
