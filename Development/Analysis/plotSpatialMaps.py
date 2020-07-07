ef5a1ae4c142205c250c67bbda40f380aeafb86d
luigiberducci <luigi.berd@gmail.com>
2020-07-07 16:03:59 +0200

    add Ge crystal in ToySpatial map to produce a more realistic map

--- Development/Analysis/plotSpatialMaps.py
+++ Development/Analysis/plotSpatialMaps.py
@@ -20,7 +20,7 @@ ge_x, ge_y = make_circle(235)
 
 # compute center of ge crystals
 n_ge_crystals = 14
-ge_angle_centers = np.linspace(0, np.pi * 2.0, n_ge_crystals + 1)
+ge_angle_centers = np.linspace(0, np.pi * 2.0, n_ge_crystals+1)
 
 plt.plot(line_x, line_y, color='blue')
 plt.plot(inner_x, inner_y, color='blue')
@@ -29,15 +29,16 @@ plt.plot(ge_x, ge_y, color='orange')
 
 ge_cry_x, ge_cry_y = make_circle(40)
 rotation_angle = 0.22
-for ge_ang in ge_angle_centers:
+for ge_ang in ge_angle_centers[:-1]:
     x0, y0 = get_ge_center(ge_ang)
     x = np.cos(rotation_angle) * x0 - np.sin(rotation_angle) * y0
     y = np.sin(rotation_angle) * x0 + np.cos(rotation_angle) * y0
+    print("{} {}".format(x, y))
     plt.scatter(x, y, color='red')
     plt.plot(x + ge_cry_x, y + ge_cry_y, color='red')
 
-x0, y0 = 350, 0
-for a in np.linspace(0, 2*np.pi, 100):
+x0, y0 = 700, 0
+for a in np.linspace(0, 2*np.pi, 500):
     x1, y1 = x0 + np.cos(a), y0 + np.sin(a)
     m = (y1 - y0) / (x1 - x0)
     x = np.linspace(-300, +700, 300)
