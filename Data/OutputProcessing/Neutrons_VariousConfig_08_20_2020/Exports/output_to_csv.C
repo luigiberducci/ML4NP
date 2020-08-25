5cfcac3b5083a6d180e601cf69477a185b8843bb
luigiberducci <luigi.berd@gmail.com>
2020-08-25 15:18:16 +0200

    analysis of simulation files, directly from ar41 export to csv

---
+++ Data/OutputProcessing/Neutrons_VariousConfig_08_20_2020/Exports/output_to_csv.C
@@ -0,0 +1,74 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  macro.C
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/02/2020 12:58:21
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Luigi Berducci (), berducci.1647887@studenti.uniroma1.it
+ *        Company:  University of Rome La Sapienza
+ *
+ * =====================================================================================
+ */
+#include<assert.h>
+
+
+void output_to_csv(TString inFilepath){
+    // IO params
+    TFile fileIn(inFilepath);
+    TObjArray *filepath = inFilepath.Tokenize("/");     // Assume unix-like path
+    TString outFileBase = ((TObjString *)(filepath->At(filepath->GetEntries() - 1)))->String();
+    TObjArray *filename= outFileBase.Tokenize(".");
+    outFileBase = ((TObjString *)(filename->At(0)))->String();
+    // Tree
+    TTree* theTree = nullptr;
+    TTreeReader theReader("fTree", &fileIn);
+    // Branches
+    TTreeReaderValue<Int_t> pid(theReader, "PID");
+    TTreeReaderValue<Int_t> p_trace_id(theReader, "ParentTrackID");
+    TTreeReaderValue<Double_t> energydeposition(theReader, "energydeposition");
+    TTreeReaderValue<Double_t> kineticenergy(theReader, "kineticenergy");
+    TTreeReaderValue<Double_t> time(theReader, "time");
+    TTreeReaderValue<Double_t> x(theReader, "x");
+    TTreeReaderValue<Double_t> y(theReader, "y");
+    TTreeReaderValue<Double_t> z(theReader, "z");
+    TTreeReaderValue<Int_t> eventnumber(theReader, "eventnumber");
+    TTreeReaderValue<Int_t> tracknumber(theReader, "tracknumber");
+    TTreeReaderValue<string> creatorprocess(theReader, "creatorprocess");
+    TTreeReaderValue<string> material(theReader, "material");
+    TTreeReaderValue<Int_t> sensitiveVolumeID(theReader, "SensitiveVolumeID");
+
+    // Loop over entries
+    int i=0;
+    Int_t maxNumberEntriesInFile = 2500000;
+    Int_t outFileNumber = 0;          // part id (for out filename)
+    std::ofstream out;
+    while(theReader.Next()){
+        Int_t cur_event = *eventnumber;
+        // Check if we have a new event and reached the max num in curr file
+	if(i % maxNumberEntriesInFile == 0){
+            outFileNumber += 1;
+            std::cout << "[Info] Creating " << outFileBase + "_part" + outFileNumber + ".csv" << endl;
+            if(i > 0)    out.close();
+	    out.open(outFileBase + "_part" + outFileNumber + ".csv");
+            //Print header
+            out << "PID,ParentTrackID,energydeposition,kineticenergy,time,x,y,z,";
+            out << "eventnumber,tracknumber,creatorprocess,";
+            out << "material,SensitiveVolumeID";
+            out << endl;
+	    }
+        i++;
+        out << *pid << "," << *p_trace_id << ",";
+        out << setprecision(20) <<  *energydeposition << "," << *kineticenergy << "," << *time << ",";
+        out << *x << "," << *y << "," << *z << ",";
+        out << *eventnumber << "," << *tracknumber  << "," << *creatorprocess << ",";
+        out << *material << "," << *sensitiveVolumeID;
+        out << endl;
+   }
+   std::cout << "\n[Info] Written " << i << " entries in " << outFileNumber << " files.\n";
+}
